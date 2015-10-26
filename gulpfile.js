ead3e71d631396ebd6ff4858ab478f7c61f27e57
Jonny Levy <iamjonnylevy@gmail.com>
2015-10-26 18:10:16 +0000

    style(gulp): convert tabs to spaces

--- gulpfile.js
+++ gulpfile.js
@@ -15,16 +15,16 @@ var srcFolder = "assets/";
  * Clean all destination folders
  */
 gulp.task('clean', function(cb) {
-	return del([
-		destFolder + '_js',
-		destFolder + '_css',
-		destFolder + '_img',
-		destFolder + '_fonts',
-		destFolder + '_ng-partials',
-		destFolder + '_data'
-	], cb)
+    return del([
+        destFolder + '_js',
+        destFolder + '_css',
+        destFolder + '_img',
+        destFolder + '_fonts',
+        destFolder + '_ng-partials',
+        destFolder + '_data'
+    ], cb)
 
-	return gulp.pipe( plugins.if( isMac, plugins.notify('Clean task complete') ) );
+    return gulp.pipe( plugins.if( isMac, plugins.notify('Clean task complete') ) );
 });
 
 /**
@@ -32,40 +32,40 @@ gulp.task('clean', function(cb) {
  * This does not clean the vendor folder.
  */
 gulp.task('clean-dev', function(cb) {
-	return del([
-		destFolder + '_js/site',
-		destFolder + '_css',
-		destFolder + '_img',
-		destFolder + '_fonts',
-		destFolder + '_ng-partials',
-		destFolder + '_data'
-	], cb)
+    return del([
+        destFolder + '_js/site',
+        destFolder + '_css',
+        destFolder + '_img',
+        destFolder + '_fonts',
+        destFolder + '_ng-partials',
+        destFolder + '_data'
+    ], cb)
 
-	return gulp.pipe( plugins.if( isMac, plugins.notify('Clean (dev) task complete') ) );
+    return gulp.pipe( plugins.if( isMac, plugins.notify('Clean (dev) task complete') ) );
 });
 
 /**
  * Install Bower components
  */
 gulp.task('bower', function() {
-  	return plugins.bower();
+    return plugins.bower();
 });
 
 /**
  * Css generation
  */
 gulp.task('styles', function() {
-	return gulp
-			.src(srcFolder + 'sass/style.scss')
-			.pipe(plugins.sass({ errLogToConsole: true }))
-			.pipe(plugins.plumber())
-			.pipe(plugins.autoprefixer({
-				browsers: ['last 2 versions', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4']
-			}))
-			.pipe(gulp.dest(destFolder + '_css/'))
-			.pipe(plugins.minifyCss({ keepSpecialComments: 0 }))
-			.pipe(plugins.rename({ extname: '.min.css' }))
-			.pipe(gulp.dest(destFolder + '_css/'));
+    return gulp
+            .src(srcFolder + 'sass/style.scss')
+            .pipe(plugins.sass({ errLogToConsole: true }))
+            .pipe(plugins.plumber())
+            .pipe(plugins.autoprefixer({
+                browsers: ['last 2 versions', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4']
+            }))
+            .pipe(gulp.dest(destFolder + '_css/'))
+            .pipe(plugins.minifyCss({ keepSpecialComments: 0 }))
+            .pipe(plugins.rename({ extname: '.min.css' }))
+            .pipe(gulp.dest(destFolder + '_css/'));
 });
 
 
@@ -74,15 +74,15 @@ gulp.task('styles', function() {
  */
 gulp.task('vendor-scripts', ['bower'], function() {
   return gulp
-  			.src([
-  				bowerFolder + 'angular/angular.min.js'
-  			])
-			.pipe(plugins.plumber())
-			.pipe(plugins.concat('vendor.js'))
-			.pipe(gulp.dest(destFolder + '_js/vendor/'))
-			.pipe(plugins.uglify())
-			.pipe(plugins.rename({suffix: '.min'}))
-			.pipe(gulp.dest(destFolder + '_js/vendor/'));
+            .src([
+                bowerFolder + 'angular/angular.min.js'
+            ])
+            .pipe(plugins.plumber())
+            .pipe(plugins.concat('vendor.js'))
+            .pipe(gulp.dest(destFolder + '_js/vendor/'))
+            .pipe(plugins.uglify())
+            .pipe(plugins.rename({suffix: '.min'}))
+            .pipe(gulp.dest(destFolder + '_js/vendor/'));
 });
 
 /**
@@ -90,17 +90,17 @@ gulp.task('vendor-scripts', ['bower'], function() {
  */
 gulp.task('scripts', function() {
   return gulp
-  			.src([
-  				srcFolder + 'js/**/*.js'
-  			])
-			.pipe(plugins.plumber())
-			.pipe(plugins.jshint())
-			.pipe(plugins.jshint.reporter('default'))
-			.pipe(plugins.concat('site.js'))
-			.pipe(gulp.dest(destFolder + '_js/site/'))
-			.pipe(plugins.uglify())
-			.pipe(plugins.rename({suffix: '.min'}))
-			.pipe(gulp.dest(destFolder + '_js/site/'));
+            .src([
+                srcFolder + 'js/**/*.js'
+            ])
+            .pipe(plugins.plumber())
+            .pipe(plugins.jshint())
+            .pipe(plugins.jshint.reporter('default'))
+            .pipe(plugins.concat('site.js'))
+            .pipe(gulp.dest(destFolder + '_js/site/'))
+            .pipe(plugins.uglify())
+            .pipe(plugins.rename({suffix: '.min'}))
+            .pipe(gulp.dest(destFolder + '_js/site/'));
 });
 
 /**
@@ -108,8 +108,8 @@ gulp.task('scripts', function() {
  */
 gulp.task('copy-images', function() {
   return gulp
-  			.src(srcFolder + 'img/**/*')
-			.pipe(gulp.dest(destFolder + '_img'));
+            .src(srcFolder + 'img/**/*')
+            .pipe(gulp.dest(destFolder + '_img'));
 });
 
 /**
@@ -117,26 +117,26 @@ gulp.task('copy-images', function() {
  */
 gulp.task('copy-fonts', function() {
   return gulp
-  			.src(srcFolder + 'fonts/**/*')
-			.pipe(gulp.dest(destFolder + '_fonts'));
+            .src(srcFolder + 'fonts/**/*')
+            .pipe(gulp.dest(destFolder + '_fonts'));
 });
 
 /**
  * Copy angular partials from src to dest
  */
 gulp.task('copy-ng-partials', function() {
-	return gulp
-			.src(srcFolder + 'ng-partials/**/*')
-			.pipe(gulp.dest(destFolder + '_ng-partials'));
+    return gulp
+            .src(srcFolder + 'ng-partials/**/*')
+            .pipe(gulp.dest(destFolder + '_ng-partials'));
 });
 
 /**
  * Copy data files from src to dest
  */
 gulp.task('copy-data', function() {
-	return gulp
-			.src(srcFolder + 'data/**/*')
-			.pipe(gulp.dest(destFolder + '_data'));
+    return gulp
+            .src(srcFolder + 'data/**/*')
+            .pipe(gulp.dest(destFolder + '_data'));
 });
 
 
@@ -144,29 +144,29 @@ gulp.task('copy-data', function() {
  * Watch various files and run appropriate task
  */
 gulp.task('watch', function() {
-	// Watch this file
-	gulp.watch('gulpfile.js', ['dev']);
+    // Watch this file
+    gulp.watch('gulpfile.js', ['dev']);
 
-	// Watch .scss files
-	gulp.watch(srcFolder + 'scss/**/*.scss', ['styles']);
+    // Watch .scss files
+    gulp.watch(srcFolder + 'scss/**/*.scss', ['styles']);
 
-	// Watch .js files
-	gulp.watch(srcFolder + 'js/**/*.js', ['scripts']);
+    // Watch .js files
+    gulp.watch(srcFolder + 'js/**/*.js', ['scripts']);
 
-	// Watch Angular partials
-	gulp.watch(srcFolder + 'ng-partials/**/*.html', ['copy-ng-partials']);
+    // Watch Angular partials
+    gulp.watch(srcFolder + 'ng-partials/**/*.html', ['copy-ng-partials']);
 
-	// Watch image files
-	gulp.watch(srcFolder + 'img/**/*', ['copy-images']);
+    // Watch image files
+    gulp.watch(srcFolder + 'img/**/*', ['copy-images']);
 
-	// Watch font files
-	gulp.watch(srcFolder + 'fonts/**/*', ['copy-fonts']);
+    // Watch font files
+    gulp.watch(srcFolder + 'fonts/**/*', ['copy-fonts']);
 
-	// Watch data files
-	gulp.watch(srcFolder + 'data/**/*', ['copy-data']);
+    // Watch data files
+    gulp.watch(srcFolder + 'data/**/*', ['copy-data']);
 
-	// Watch bower files
-	gulp.watch('bower.json', ['vendor-scripts']);
+    // Watch bower files
+    gulp.watch('bower.json', ['vendor-scripts']);
 
 });
 
@@ -174,7 +174,7 @@ gulp.task('watch', function() {
  * Default task. Runs all other tasks.
  */
 gulp.task('default', ['clean'], function() {
-	return runSequence('vendor-scripts', 'scripts', 'styles', 'copy-images', 'copy-fonts', 'copy-ng-partials', 'copy-data');
+    return runSequence('vendor-scripts', 'scripts', 'styles', 'copy-images', 'copy-fonts', 'copy-ng-partials', 'copy-data');
 });
 
 /**
@@ -182,5 +182,5 @@ gulp.task('default', ['clean'], function() {
  * Requires 'vendor-scripts' task to have been run once first.
  */
 gulp.task('dev', ['clean-dev'], function() {
-	return runSequence('scripts', 'styles', 'copy-images', 'copy-fonts', 'copy-ng-partials', 'copy-data');
+    return runSequence('scripts', 'styles', 'copy-images', 'copy-fonts', 'copy-ng-partials', 'copy-data');
 });
