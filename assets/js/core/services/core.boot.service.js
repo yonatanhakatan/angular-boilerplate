51e12d0271c1d59862cc4e37ab51d83778746258
Jonny Levy <iamjonnylevy@gmail.com>
2015-11-01 21:45:23 +0000

    chore(core): add core module

---
+++ assets/js/core/services/core.boot.service.js
@@ -0,0 +1,19 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('app.core')
+        .service('coreBootService', coreBootService);
+
+    coreBootService.$inject = [];
+
+    function coreBootService() {
+        /*jshint validthis: true */
+        this.init = init;
+
+        function init() {
+            // Tasks that need to happen when the
+            // app launches should be written here
+        }
+    }
+})();
